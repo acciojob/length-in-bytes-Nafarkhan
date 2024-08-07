b0600cacaf9b2fada9e87126e52b0e54f96b0f3d
Nafar Khan <147840894+Nafarkhan@users.noreply.github.com>
2024-08-07 15:05:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const byteSize = (str) => {
-  // write your code here
+    // Create a Blob object with the string
+    const blob = new Blob([str]);
+    // Return the size of the Blob in bytes
+    return blob.size;
 };
 
-// Do not change the code below
-const str = prompt("Enter some string.");
-alert(byteSize(str));
+const showByteSize = () => {
+    const str = document.getElementById('inputString').value;
+    alert(`Byte size: ${byteSize(str)}`);
+};
